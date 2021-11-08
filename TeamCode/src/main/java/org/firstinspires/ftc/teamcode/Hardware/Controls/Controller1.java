e47674c90fe2bb0bdee3078311e8c950b4f3c8cd
LQGIK <45301020+LQGIK@users.noreply.github.com>
2021-11-08 17:01:59 -0500

    it just works

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/Controls/Controller1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/Controls/Controller1.java
@@ -2,29 +2,111 @@ package org.firstinspires.ftc.teamcode.Hardware.Controls;
 
 import com.qualcomm.robotcore.hardware.Gamepad;
 
+import org.opencv.core.Point;
+
 public class Controller1 {
-    public Button circle;
-    public Button LB;
-    public Button LS;
-    public Controller1(Gamepad gamepad){
-     /*
-        Y O U R    C O D E    H E R E
-     */
+
+    private final Gamepad gamepad;
+
+    public Controller1(Gamepad gamepad) {
+        this.gamepad = gamepad;
+        cross = new Button();
+        circle = new Button();
+        triangle = new Button();
+        square = new Button();
+        up = new Button();
+        down = new Button();
+        left = new Button();
+        right = new Button();
+        RB = new Button();
+        LB = new Button();
+        RS = new Button();
+        LS = new Button();
+        share = new Button();
+        touchpad = new Button();
+        RTrigger = new Button();
+        LTrigger = new Button();
+
+
     }
 
-    public class Button {
 
-        public Button(){
-        /*
-        Y O U R    C O D E    H E R E
-        */
+    public Button cross,circle,triangle,square,up,down,left,right,RB,LB,RS,LS,share,touchpad,RTrigger,LTrigger;
+
+
+
+    public class Button{
+
+        private boolean IsPressed = false;
+        private boolean LastPressed = false;
+        private boolean Toggle = false;
+        private boolean IsTapped = false;
+        float value = 0;
+
+        public void Update(boolean _button){
+            IsPressed = _button;
+
+            if (IsPressed && !LastPressed){
+                IsTapped = true;
+            }else{
+                IsTapped = false;
+            }
+
+            if (IsTapped){
+                Toggle = !Toggle;
+            }
+            LastPressed = IsPressed;
         }
 
-        public boolean press(){
-        /*
-        Y O U R    C O D E    H E R E
-        */
-            return true;
+        public void Update(float inputFloat){
+            value = inputFloat;
+            Update(inputFloat >= .7);
         }
+
+
+        public float getValue() {
+            return value;
+        }
+
+        public boolean onPress(){ return IsPressed; }
+
+        public boolean onToggle(){ return Toggle; }
+
+        public boolean onTap(){ return   IsTapped; }
+
+
+
+
+
     }
+    public void ControllerUpdate(){
+        cross.Update(gamepad.cross);
+        circle.Update(gamepad.circle);
+        triangle.Update(gamepad.triangle);
+        square.Update(gamepad.square);
+        up.Update(gamepad.dpad_up);
+        down.Update(gamepad.dpad_down);
+        left.Update(gamepad.dpad_left);
+        right.Update(gamepad.dpad_right);
+        RB.Update(gamepad.right_bumper);
+        LB.Update(gamepad.left_bumper);
+        RS.Update(gamepad.right_stick_button);
+        LS.Update(gamepad.left_stick_button);
+        share.Update(gamepad.share);
+        touchpad.Update(gamepad.touchpad);
+        RTrigger.Update(gamepad.right_trigger);
+        LTrigger.Update(gamepad.left_trigger);
+
+
+    }
+
+    public Point leftStick(){
+        return new Point(gamepad.left_stick_x,gamepad.left_stick_y);
+    }
+    public Point rightStick(){
+        return new Point(-gamepad.right_stick_x,-gamepad.right_stick_y);
+    }
+
 }
+
+
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/IterativeTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/IterativeTeleOp.java
@@ -5,21 +5,44 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
+import org.firstinspires.ftc.teamcode.Hardware.Controls.Controller1;
+import org.firstinspires.ftc.teamcode.Hardware.Mecanum;
+import org.firstinspires.ftc.teamcode.Hardware.Mecanum2;
+import org.firstinspires.ftc.teamcode.Hardware.Sensors.IMU;
+import org.firstinspires.ftc.teamcode.Utilities.MathUtils;
+import org.firstinspires.ftc.teamcode.Utilities.PID;
+import org.firstinspires.ftc.teamcode.Utilities.PIDWeights;
+
 import static org.firstinspires.ftc.teamcode.Utilities.OpModeUtils.multTelemetry;
 import static org.firstinspires.ftc.teamcode.Utilities.OpModeUtils.setOpMode;
 
-@Disabled
+
 @TeleOp(name="Iterative TeleOp", group="Iterative Opmode")
 public class IterativeTeleOp extends OpMode {
 
     // Declare OpMode members.
     private ElapsedTime runtime = new ElapsedTime();
+    Mecanum2 evansChassis;
+    double power;
+    PID evansChassisPid;
+    Controller1 controller;
+    IMU imu;
+    private double setPoint = 0;
+    private boolean wasTurning;
+
+
     /*
      * Code to run ONCE when the driver hits INIT
      */
     @Override
     public void init() {
         setOpMode(this);
+
+        power = 0.6;
+        imu = new IMU("imu");
+        evansChassis = new Mecanum2();
+        evansChassisPid = new PID(PIDWeights.proportionalWeight, PIDWeights.integralWeight, PIDWeights.derivativeWeight);
+        controller = new Controller1(gamepad1);
 
         multTelemetry.addData("Status", "Initialized");
         multTelemetry.update();
@@ -31,9 +54,7 @@ public class IterativeTeleOp extends OpMode {
     @Override
     public void init_loop() {
 
-        /*
-                    Y O U R   C O D E   H E R E
-                                                   */
+        imu.getAngle();
 
 
         multTelemetry.addData("Status", "InitLoop");
@@ -62,11 +83,29 @@ public class IterativeTeleOp extends OpMode {
     @Override
     public void loop() {
 
-        /*
+        double correction = evansChassisPid.update(imu.getAngle() - setPoint, true);
+        double rotation;
+        if(!(controller.rightStick().x == 0)){
+            rotation = -controller.rightStick().x;
+            wasTurning = true;
+        }else{
+            if(wasTurning){
+                setPoint = imu.getAngle();
+                wasTurning = false;
+            }
+            rotation = correction;
+        }
 
-                    Y O U R   C O D E   H E R E
+        if(controller.RTrigger.onPress()){
+            power = 0.3;
+        }else{
+            power = 0.6;
+        }
 
-                                                    */
+        double drive = -MathUtils.shift(controller.leftStick(), imu.getAngle()).y;
+        double strafe = MathUtils.shift(controller.leftStick(), imu.getAngle()).x;
+        double turning = rotation;
+        evansChassis.setDrivePower(power,strafe,turning,drive);
 
 
 
@@ -76,8 +115,11 @@ public class IterativeTeleOp extends OpMode {
         /*
              ----------- L O G G I N G -----------
                                                 */
-        multTelemetry.addData("Status", "TeleOp Running");
-        multTelemetry.update();
+//        multTelemetry.addData("Status", "TeleOp Running");
+//        multTelemetry.addData("Drive:", drive);
+//        multTelemetry.addData("Strafe:", strafe);
+//        multTelemetry.addData("Turning:", turning);
+//        multTelemetry.update();
     }
 
     @Override
