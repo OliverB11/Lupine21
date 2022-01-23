ead2d3d38469331d64c6326f1bc351402ba65d48
OliverB11 <92954010+OliverB11@users.noreply.github.com>
2022-01-23 17:40:32 -0500

    Oliver Jan 23

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/BaseAuto.java
@@ -0,0 +1,58 @@
+package org.firstinspires.ftc.teamcode.Autonomous;
+
+import static org.firstinspires.ftc.teamcode.Utilities.OpModeUtils.multTelemetry;
+import static org.firstinspires.ftc.teamcode.Utilities.OpModeUtils.setOpMode;
+
+import android.os.Build;
+
+import androidx.annotation.RequiresApi;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.Hardware.DuckWheel;
+import org.firstinspires.ftc.teamcode.Hardware.Intake;
+import org.firstinspires.ftc.teamcode.Hardware.Mecanum;
+import org.firstinspires.ftc.teamcode.Hardware.ScoringMechanism;
+import org.firstinspires.ftc.teamcode.Utilities.PID;
+import org.firstinspires.ftc.teamcode.Utilities.Unfixed;
+import org.firstinspires.ftc.teamcode.Z.Side;
+
+@Autonomous(name="BaseAuto", group="Autonomous Linear Opmode")
+public class BaseAuto extends LinearOpMode {
+    // Declare Things.
+    private ElapsedTime time = new ElapsedTime();
+    Mecanum robot;
+
+//Code to run when the driver hits INIT
+    public void initialize(){
+        setOpMode(this);
+
+        //This should depend on if this is a red or blue auto
+        Side.red = true;
+        Side.blue = false;
+
+        //Initialize Things
+        robot = new Mecanum();
+        ElapsedTime time = new ElapsedTime();
+    }
+
+
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    @Override
+    public void runOpMode(){
+
+        initialize();
+        waitForStart();
+
+//Code to run when the driver hits start
+        if (opModeIsActive()){
+
+
+        }
+    }
+
+
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/BaseTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/BaseTeleOp.java
@@ -8,11 +8,10 @@ import org.firstinspires.ftc.teamcode.Hardware.Controls.Controller;
 import org.firstinspires.ftc.teamcode.Hardware.DuckWheel;
 import org.firstinspires.ftc.teamcode.Hardware.Intake;
 import org.firstinspires.ftc.teamcode.Hardware.Mecanum;
-import org.firstinspires.ftc.teamcode.Hardware.Sensors.Gyro;
+import org.firstinspires.ftc.teamcode.Hardware.ScoringMechanism;
 import org.firstinspires.ftc.teamcode.Utilities.MathUtils;
 import org.firstinspires.ftc.teamcode.Z.Side;
 
-import static java.lang.Math.floorMod;
 import static org.firstinspires.ftc.teamcode.Utilities.OpModeUtils.multTelemetry;
 import static org.firstinspires.ftc.teamcode.Utilities.OpModeUtils.setOpMode;
 
@@ -20,123 +19,154 @@ import static org.firstinspires.ftc.teamcode.Utilities.OpModeUtils.setOpMode;
 @TeleOp(name="Base TeleOp", group="Iterative Opmode")
 public class BaseTeleOp extends OpMode {
 
-    // Declare OpMode members.
+    // Declare Things
     private ElapsedTime runtime = new ElapsedTime();
     public static Mecanum robot;
     double power;
     Controller controller;
+    Controller controller2;
     double setPoint = 360;
     boolean wasTurning;
 
 
-    /*
-     * Code to run ONCE when the driver hits INIT
-     */
+
+//Code to run when the driver hits INIT
     @Override
     public void init() {
         setOpMode(this);
 
+
+        //Set power and initialize things
         power = 0.6;
         robot = new Mecanum();
         controller = new Controller(gamepad1);
+        controller2 = new Controller(gamepad2);
 
-
-
-        multTelemetry.addData("Status", "Initialized");
-      //  multTelemetry.update();
+        if (!Side.blue && !Side.red) {
+            Side.blue = true;
+            Side.red = false;
+        }
     }
 
-    /*
-     * Code to run REPEATEDLY after the driver hits INIT, but before they hit PLAY
-     */
 
 
+//Code to run REPEATEDLY after the driver hits INIT, but before they hit PLAY
     @Override
     public void init_loop() {
 
-        //multTelemetry.addData("Status", "InitLoop");
 
-       // multTelemetry.update();
     }
 
-    /*
-     * Code to run ONCE when the driver hits PLAY
-     */
+
+//Code to run ONCE when the driver hits PLAY
+
     @Override
     public void start() {
         runtime.reset();
 
-
-        /*
-                    Y O U R   C O D E   H E R E
-                                                   */
-
-        multTelemetry.addData("Status", "Started");
-        multTelemetry.update();
     }
 
-    /*
-     * Code to run REPEATEDLY after the driver hits PLAY but before they hit STOP
-     */
+
+//Code to run REPEATEDLY after the driver hits PLAY but before they hit STOP
     @Override
     public void loop() {
-        //Declarations
+
+//In-Loop variable declarations
         double rotation;
 
 
-        // Updates
+// Updates (Don't forget these)
         controller.controllerUpdate();
+        controller2.controllerUpdate();
         robot.gyro.update();
 
 
+// PID
 
-        // PID
-        double correction = robot.pid.update(robot.gyro.rawAngle() - setPoint);
-
-
-        if(!(controller.rightStick().x == 0)){
+        if (!(controller.rightStick().x == 0)) {
             rotation = controller.rightStick().x;
             wasTurning = true;
-        }else if(wasTurning && robot.gyro.rateOfChange() < 4){
+        } else if (wasTurning && Math.abs(robot.gyro.rateOfChange()) > 0) {
             rotation = controller.rightStick().x;
-        }else{
-            if(wasTurning){
+        } else {
+            if (wasTurning) {
                 setPoint = robot.gyro.rawAngle();
                 wasTurning = false;
             }
-            rotation = correction;
+            rotation = robot.pid.update(robot.gyro.rawAngle() - setPoint);
         }
-        multTelemetry.addData("wasTurning", wasTurning);
-        multTelemetry.addData("rotatiion", rotation);
-        multTelemetry.addData("pidresult", correction);
 
-        // Speed Control
-        if (controller.RTrigger.press()) {
+// Speed Control
+        if (controller.RB.press()) {
             power = 0.2;
+        }else if(controller.LB.press()){
+            power = 0.5;
         } else {
-            power = 0.4;
+            power = 0.8;
         }
+
+
+//Gyro Reset ability
+        if (controller.share.tap()) {
+            setPoint = 0;
+            robot.gyro.reset();
+        }
+
+// Switch Sides
+        if(controller2.share.tap()) {
+            if (Side.blue) {
+                Side.blue = false;
+                Side.red = true;
+            } else if (Side.red) {
+                Side.blue = true;
+                Side.red = false;
+            }
+        }
+
+
+// Stuff
+
+        //Controller1 Stuff
+
+        //Controller 2 Stuff
+
+
+
+
+        //Automatic Turning
+        if (controller.left.press()) {
+            setPoint = MathUtils.closestAngle(90, robot.gyro.rawAngle());
+        }
+        if (controller.up.press()) {
+            setPoint = MathUtils.closestAngle(0, robot.gyro.rawAngle());
+        }
+        if (controller.down.press()) {
+            setPoint = MathUtils.closestAngle(180, robot.gyro.rawAngle());
+        }
+        if (controller.right.press()) {
+            setPoint = MathUtils.closestAngle(270, robot.gyro.rawAngle());
+        }
+
+
+
 
 
         //Movement control
         double drive = -MathUtils.shift(controller.leftStick(), robot.gyro.rawAngle()).y;
         double strafe = MathUtils.shift(controller.leftStick(), robot.gyro.rawAngle()).x;
-        double turn = -controller.rightStick().x;
+        double turn = -rotation;
 
-        robot.setDrivePower(power, strafe, -rotation, drive);
+        robot.setDrivePower(power, strafe, turn, drive);
 
 
+        //Telemetry
 
 
     }
 
-
-
+//Code to run once when you hit stop (Normally theres nothing here)
     @Override
-    public void stop() {
+    public void stop(){
 
-        /*
-                    Y O U R   C O D E   H E R E
-                                                   */
     }
 }
